45c21e22d87553aa935807e2a92c6be0b78b2649
voganhaov <152450101+voganhaov@users.noreply.github.com>
2023-12-08 06:54:55 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,43 @@
+def generate_random_data():
+    main()
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    for item in data:
+
+def main():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    return data
+
+    main()
+    data = generate_random_data()
+if __name__ == "__main__":
+import random
+    return data
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+
+def main():
+    for item in data:
+def generate_random_data():
+    });
+    data.forEach(item => {
+}
+function generateRandomData() {
+function main() {
+    const data = generateRandomData();
+    const data = [];
+
+}
+    }
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+    return data;
