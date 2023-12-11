772a09d530704d100911e41c7c821261954907ed
voganhaov <152450101+voganhaov@users.noreply.github.com>
2023-12-11 04:36:29 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,65 +1,30 @@
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    });
 
-    for item in data:
-
-import random
-
-        print(f"Random Number: {item}")
-    main()
-def generate_random_data():
-    data = generate_random_data()
-    return data
-if __name__ == "__main__":
-        }
-        int[] data = new int[10];
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-            data[i] = (int) (Math.random() * 100) + 1;
-
-    }
-
-        }
 }
-    public static void main(String[] args) {def generate_random_data():
-    for item in data:
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
+    return data;
+function main() {
 
-    data = [random.randint(1, 100) for _ in range(10)]
+        console.log("Random Number: " + item);
+    }
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+main();
+
+}
+function generateRandomData() {
+    const data = generateRandomData();def generate_random_data():
+import random
+
+
     return data
-
-    data = generate_random_data()
-
-
-def main():
-import random
-    main()    return data
-import random
-def generate_random_data():
     for item in data:
 
-
-    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
 if __name__ == "__main__":
+def main():
+    data = generate_random_data()
 
     main()
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-
-def main():
-if __name__ == "__main__":
-    return data
-    data = generate_random_data()
-    main()
-    for item in data:
-
-
-import random
-        print(f"Random Number: {item}")
-def main():
     data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
